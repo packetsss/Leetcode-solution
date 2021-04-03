289eae950635d695784550ada5f38e8703d2c6a2
Paul Pan <pyjpanyanjunpyj@gmail.com>
2021-04-03 11:17:00 -0700

    Create 474_Ones and Zeroes.py

---
+++ Medium/474_Ones and Zeroes.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def findMaxForm(S, M, N):
+    dp = [[0 for _ in range(N + 1)] for _ in range(M + 1)]
+    for s in S:
+        zeros = s.count("0")
+        ones = len(s) - zeros
+        # print(f"Zeros: {zeros}, Ones: {ones}")
+        for i in range(M, zeros - 1, -1):
+            for j in range(N, ones - 1, -1):
+                dp[i][j] = max(dp[i][j], dp[i - zeros][j - ones] + 1)
+                # print(f"Zeros: {zeros}, Ones: {ones}")
+                # print(f"i: {i}, j: {j}\n{np.array(dp)}")
+    return dp[M][N]
+
+
+findMaxForm(["10", "0001", "111001", "1", "0"], 4, 3)
